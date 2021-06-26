6d3f9d26ff858bdcc4e32fcd84e402d5b0bc513c
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2021-06-25 22:17:54 -0500

    Se termino punto websocket de cliente

--- Proyecto_facturacion/src/java/logica/ServerCominication/EditClienteServer.java
+++ Proyecto_facturacion/src/java/logica/ServerCominication/EditClienteServer.java
@@ -1,11 +1,15 @@
 package logica.ServerCominication;
 
+import datos.DBCliente;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import logica.Cliente;
 import logica.EncoderDecoder.DecoderCliente;
 import logica.EncoderDecoder.EncoderCliente;
 
@@ -27,6 +31,22 @@ public class EditClienteServer {
     @OnClose
     public void salir(Session sesion){
         conectados.remove(sesion);
+    }
+    
+    @OnMessage
+    public void mensaje(Cliente cli, String inst) throws SQLException{
+        DBCliente cliDB = new DBCliente();
+        switch(Integer.parseInt(inst)){
+            case 1:
+                cliDB.modifyCliente(cli);
+                break;
+            case 2:
+                cliDB.insertarCliente(cli);
+                break;
+            case 3:
+                cliDB.eliminarCliente(cli.getId_cliente());
+                break;
+        }
     }
     
 }
