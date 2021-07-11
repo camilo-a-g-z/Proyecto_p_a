908c4224c00c20aeac27f2ed797f2f0f77a9b9b7
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2021-07-11 11:18:46 -0500

    Se empezo parte de edicion en edit admin

--- Proyecto_facturacion/web/js/edit_ad.js
+++ Proyecto_facturacion/web/js/edit_ad.js
@@ -3,11 +3,17 @@ var url;
 var v_selected;
 var band = true;
 var nombre;
+function onOpen() {
+    console.log('Conectado..');
+}
+function onClose() {
+    console.log('Desonectado..');
+}
 select = function () {
     var input = document.getElementById('opcion');
     input.addEventListener('input', function () {
         v_selected = this.value;
-        if (this.value == "") {
+        if (v_selected == "") {
             console.log("no hace conexion");
         } else {
             if (!band) {
@@ -15,28 +21,52 @@ select = function () {
             } else {
                 band = false;
             }
-            if (this.value == "Articulo") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/articulo';
-                console.log("Articulo");
-            } else if (this.value == "Categoria") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/categoria';
-                console.log("Categoria");
-            } else if (this.value == "Cliente") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/cliente';
-                console.log("Cliente");
-            } else if (this.value == "Factura") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/factura';
-                console.log("Factura");
-            } else if (this.value == "Metodo_pago") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/metodo_pago';
-                console.log("Metodo de pago");
-            } else if (this.value == "Ciudad") {
-                url = 'ws://localhost:8080/Proyecto_facturacion/ciudad';
-                console.log("Ciudad");
-            }
+            selecionar_url();
             procedimiento();
         }
     });
+};
+selecionar_url = function () {
+    if (v_selected == "Articulo") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/articulo';
+        console.log("Articulo");
+    } else if (v_selected == "Categoria") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/categoria';
+        console.log("Categoria");
+    } else if (v_selectede == "Cliente") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/cliente';
+        console.log("Cliente");
+    } else if (v_selected == "Factura") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/factura';
+        console.log("Factura");
+    } else if (v_selected == "Metodo_pago") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/metodo_pago';
+        console.log("Metodo de pago");
+    } else if (v_selected == "Ciudad") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/ciudad';
+        console.log("Ciudad");
+    }
+};
+selecionar_url_modify = function () {
+    if (v_selected == "Articulo") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyArticulo';
+        console.log("Articulo");
+    } else if (v_selected == "Categoria") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyCategoria';
+        console.log("Categoria");
+    } else if (v_selectede == "Cliente") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyCliente';
+        console.log("Cliente");
+    } else if (v_selected == "Factura") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyFactura';
+        console.log("Factura");
+    } else if (v_selected == "Metodo_pago") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyMP';
+        console.log("Metodo de pago");
+    } else if (v_selected == "Ciudad") {
+        url = 'ws://localhost:8080/Proyecto_facturacion/modifyCiudad';
+        console.log("Ciudad");
+    }
 };
 procedimiento = function () {
     ws = null;
@@ -51,12 +81,7 @@ procedimiento = function () {
     var info = document.getElementById('informacion');
     var boton_2 = document.getElementById('boton');
 
-    function onOpen() {
-        console.log('Conectado..');
-    }
-    function onClose() {
-        console.log('Desonectado..');
-    }
+
     function onMessage(evt) {
         console.log("recivido");
         var obj = JSON.parse(evt.data);
@@ -120,5 +145,44 @@ enviar = function () {
     } else {
         ws.send(nombre.value);
     }
+};
+close_conexion = function () {
+    ws.close();
+    selecionar_url_modify();
+    ws = null;
+    ws = new WebSocket(url);
+    ws.onopen = onOpen;
+    ws.onclose = onClose;
+    setTimeout('enviar_edit()', 3000);
 }
-    
+enviar_edit = function () {
+    console.log("Entro");
+
+    if (v_selected == "Articulo") {
+        var msg = {
+            id_articulo: "1",
+            nombre: document.getElementById("input_1").value,
+            cant_stock: document.getElementById("input_2").value,
+            descripcion: document.getElementById("input_3").value,
+            id_categoria: document.getElementById("input_4").value,
+            mensaje: '2'
+        };
+        ws.send(JSON.stringify(msg));
+    } else if (v_selected == "Categoria") {
+        var msg = {
+            id_categoria: "1",
+            nombre: document.getElementById("input_1").value,
+            descripcion: document.getElementById("input_2").value,
+            mensaje: '2'
+        };
+        ws.send(JSON.stringify(msg));
+    } else if (v_selected == "Cliente") {
+
+    } else if (v_selected == "Factura") {
+
+    } else if (v_selected == "Metodo_pago") {
+
+    } else if (v_selected == "Ciudad") {
+
+    }
+};
